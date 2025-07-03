4f8b7e207231a34bc11d7f6946efabb126f9ac73
dlacrzz <122063410+dlacrzz@users.noreply.github.com>
2025-07-03 13:48:29 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/home.html');
+});
+
+app.get('/home', (req, res) => {
+  res.sendFile(__dirname + '/home.html');
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/about.html');
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/contact.html');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
